707ea3328c16878ec78150319dddac54b0cf308e
Alexey Kolesnikov <koleslex@gmail.com>
2018-09-02 23:51:44 +0600

    Auto-generated commit

--- src/app/pipes/tour-filter.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { TourFilterPipe } from './tour-filter.pipe';
-
-describe('TourFilterPipe', () => {
-  it('create an instance', () => {
-    const pipe = new TourFilterPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
